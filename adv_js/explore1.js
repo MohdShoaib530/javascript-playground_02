694b67554e6b0b1d996cf0105867758f76b1c0d2
MohdShoaib530 <mohdshoaib91530@gmail.com>
2025-01-28 20:50:22 +0530

    add explore1.js

---
+++ adv_js/explore1.js
@@ -0,0 +1,12 @@
+console.log('hello my name is mohd shoaib');
+
+function sayHello() {
+  console.log('hahahaha');
+}
+setTimeout(() => {
+  sayHello();
+}, 2000);
+
+for (let index = 0; index < 10; index++) {
+  console.log(`index is ${index}`);
+}
